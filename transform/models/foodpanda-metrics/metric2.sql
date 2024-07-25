34c1cacd9dea1c062ad40de2cc08836f297e3c1e
Zunayed <zunayedology@gmail.com>
2024-07-25 23:43:43 +0600

    dbt model for metric 2 added

---
+++ transform/models/foodpanda-metrics/metric2.sql
@@ -0,0 +1,10 @@
+{{ config(materialized = "view") }}
+
+WITH min_one_successful_order_customers AS (
+    SELECT
+        COUNT(DISTINCT customer_id) AS total_customers
+    FROM {{ ref("orders") }}
+    WHERE is_successful_order = TRUE
+)
+
+SELECT * FROM min_one_successful_order_customers;
