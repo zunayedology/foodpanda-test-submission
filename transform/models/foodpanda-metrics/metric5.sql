e40905b2fe85524b7870a3213cccd8f4c6aab050
Zunayed <zunayedology@gmail.com>
2024-07-26 22:43:09 +0600

    dbt model of metric 5 added

---
+++ transform/models/foodpanda-metrics/metric5.sql
@@ -0,0 +1,46 @@
+{{ config(materialized = "view") }}
+
+WITH
+    customer_orders AS (
+        SELECT customer_id,
+               date_local
+        FROM
+            {{ ref("orders") }}
+        WHERE
+            is_successful_order = TRUE
+    ),
+    last_order_date AS (
+        SELECT
+            MAX(date_local) AS last_date
+        FROM
+            customer_orders
+    ),
+    customer_with_prev_orders AS (
+        SELECT
+            DISTINCT customer_id
+        FROM
+            customer_orders
+        WHERE
+            date_local < DATE_SUB(
+                    (SELECT last_date FROM last_order_date),
+                    INTERVAL 7 DAY
+                )
+    ),
+    reordering_customers AS (
+        SELECT
+            DISTINCT customer_id
+        FROM
+            customer_orders
+        WHERE
+            date_local >= DATE_SUB(
+                    (SELECT last_date FROM last_order_date),
+                    INTERVAL 7 DAY
+                )
+    )
+
+SELECT
+    COUNT(DISTINCT reordering_customers.customer_id) AS total_reordering_customers
+FROM
+    reordering_customers
+JOIN customer_with_prev_orders
+    ON reordering_customers.customer_id = customer_with_prev_orders.customer_id;
